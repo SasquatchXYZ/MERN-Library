980391b6f3c66c063303b757ea1e5114a367ee4c
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-14 18:26:46 -0500

    Initial Commit of files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define Middleware Here -------------------------------------
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Serve Up Static Assets (usually on Heroku) -----------------
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+// Define API Routes Here -------------------------------------
+
+// Send every other request to the React App
+// Define any API routes before this runs
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`API Server now on port: ${PORT}`)
+});
