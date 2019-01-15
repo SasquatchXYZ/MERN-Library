c78e7ececbee221470aba1b4da1deb28ba37d747
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-14 19:20:53 -0500

    Commence building the library

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container text-center">
+        <a className="navbar-brand" href="/">
+          MERN Digital Library
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
